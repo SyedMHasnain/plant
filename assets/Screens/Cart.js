8ce9d0d84e812bbc0e40e3f0978772cb8377493b
SyedMhasnian <husnain486@gmail.com>
2024-01-28 22:06:16 +0500

    ad
    a

--- assets/Screens/Cart.js
+++ assets/Screens/Cart.js
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 import LottieView from 'lottie-react-native';
 import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
 import colors from '../theme/colors';
 const Cart = () => {
- 
   return (
     <View style={styles.container}>
       <LottieView
@@ -17,22 +16,22 @@ const Cart = () => {
         loop
       />
 
-      <Text style={styles.Text}>{item.title }Your Cart Is Empty </Text>
+      <Text style={styles.Text}>{item.title}Your Cart Is Empty </Text>
     </View>
   );
-}
+};
 
-export default Cart
+export default Cart;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  Text:{  
-    fontSize:moderateScale(20),
-    fontFamily:'Montserrat-Bold',
-    color:colors.catcolor
+  Text: {
+    fontSize: moderateScale(20),
+    fontFamily: 'Montserrat-Bold',
+    color: colors.catcolor,
   },
 });
--- assets/Screens/Home.js
+++ assets/Screens/Home.js
@@ -1,32 +1,26 @@
 import {StyleSheet, ScrollView, Text, View, Button} from 'react-native';
-import React from 'react'
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Categories from '../components/Categories';
 import colors from '../theme/colors';
 import ItemDrawer from '../components/ItemDrawer';
 
-
-const Home = (props ) => {
+const Home = props => {
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.Container}>
       <Navbar />
       <Categories />
       <View>
-        <ItemDrawer 
-        navigation={props.navigation}
-        route={props.route}
-          />
+        <ItemDrawer navigation={props.navigation} route={props.route} />
       </View>
     </ScrollView>
   );
-}
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   Container: {
- 
-      backgroundColor:colors.white,
-      
+    backgroundColor: colors.white,
   },
 });
